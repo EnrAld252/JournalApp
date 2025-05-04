ceacd9ae489d88668468de66d4fd660ce2ad5f55
EnrAld252 <enriqueDev10@gmail.com>
2025-05-03 23:43:02 -0600

    docs creado

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -20,11 +20,11 @@ export const AppRouter = () => {
        {/* JournalApp  */}
       {
         (status === 'authenticated')
-          ? <Route path='./*' element={<JournalRoutes />} />
-          : <Route path='./auth/*' element={<AuthRoutes />} />
+          ? <Route path='/*' element={<JournalRoutes />} />
+          : <Route path='/auth/*' element={<AuthRoutes />} />
       }
       {/* En caso de que no este autentificado mandara a login  */}
-      <Route path="/*" element={<Navigate to="./auth/login" />} />
+      <Route path="/*" element={<Navigate to="/auth/login" />} />
     </Routes>
   )
 }
